96fbd9d98ce719e63fb23cf7a64ac33e351ea409
Allison Mackey <a.mackey693@gmail.com>
2020-03-31 16:26:10 -0700

    adds classes to checkbox options, adds span id's, and if/ else if statements to jQuery

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
-  $("form#stress").submit(function(){
+  $("form#stress-test").submit(function(event){
+    event.preventDefault();
+    $("#stress").show();
+
+    $("input:checkbox[name=option]:checked").each(function(){
+      var warning = parseInt($(".warning").val());
+      var stress = parseInt($(".stress").val());
+      var symptoms = parseInt($(".symptoms").val());
+      console.log (warning, stress, symptoms)
+      var result = (warning + stress + symptoms);
+
+      if (result <= 6) {
+        $("#low-stress").show();
+      } else if (result <= 10) {
+        $("#med-stress").show();
+      } else if (result >=11) {
+        $("#high-stress").show();
+      }
+      
+    });
     
 
   });
